0ba9d8cf48a7ef75f151f06675b4957f422cd0c1
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-17 17:15:48 +0300

    Added getSurveys function

--- laravel-survey/app/Http/Controllers/AdminController.php
+++ laravel-survey/app/Http/Controllers/AdminController.php
@@ -9,29 +9,41 @@ use App\Models\Question;
 
 class AdminController extends Controller
 {
-    public function addSurvey(Request $request){
+    public function addSurvey(Request $request)
+    {
         $survey = new Survey();
         $survey->name = $request->name;
         $survey->save();
-        
+
         return response()->json([
             "status" => "Survey Added"
         ], 200);
     }
-        public function addQuestion(Request $request){
-            $question = new Question();
-            $question->question = $request->question;
-            $question->type = $request->type;
-            $question->survey_id = $request->survey_id;
-            $question->choices = $request->choices;
-            $question->save();
-            
-            return response()->json([
-                "status" => "Question Added"
-            ], 200);
+    public function addQuestion(Request $request)
+    {
+        $question = new Question();
+        $question->question = $request->question;
+        $question->type = $request->type;
+        $question->survey_id = $request->survey_id;
+        $question->choices = $request->choices;
+        $question->save();
 
+        return response()->json([
+            "status" => "Question Added"
+        ], 200);
+    }
+
+    public function getSurveys($id = null)
+    {
+        if ($id != null) {
+            $surveys = Survey::find($id);
+        } else {
+            $surveys = Survey::all();
         }
 
-      
-
+        return response()->json([
+            "status" => "Success",
+            "Surveys: " => $surveys
+        ], 200);
+    }
 }
