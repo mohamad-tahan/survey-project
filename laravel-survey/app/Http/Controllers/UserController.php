d73aece7841f1c8fd694261c02797c96d78e34d8
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-17 17:08:07 +0300

    Added addAnswer function

--- laravel-survey/app/Http/Controllers/UserController.php
+++ laravel-survey/app/Http/Controllers/UserController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Answer;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function addAnswer(Request $request){
+        $answers = new Answer();
+        $answers->answer = $request->answer;
+        $answers->question_id = $request->question_id;
+        $answers->user_id = $request->user_id;
+       
+        $answers->save();
+        
+        return response()->json([
+            "status" => "Answers Added"
+        ], 200);
+
+    }
 }
