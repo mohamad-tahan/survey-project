7964f6374df1f3d32ec06ad1d9047780eebe9cbf
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-18 20:56:50 +0300

    Added routes in app.js

--- react-survey/src/App.js
+++ react-survey/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Login from './components/Login';
+import Test from './components/Test';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import AddQuestions from './components/AddQuestions';
+import Navbar from './components/Navbar';
+import AddSurvey from './components/AddSurvey';
+
+
+
+
 
 function App() {
+  
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    
+      <BrowserRouter>
+       <div className="App">
+
+        <Routes>
+
+        <Route path="/login" element={<Login />}></Route>
+
+          <Route path="/addSurvey" element={<AddSurvey /> }></Route>
+          <Route path="/addQuestions" element={<AddQuestions /> }></Route>
+          <Route path="/test" element={<Test /> }></Route>
+
+        </Routes>
+
+        </div>
+      </BrowserRouter>
   );
 }
 
-export default App;
+export default App
