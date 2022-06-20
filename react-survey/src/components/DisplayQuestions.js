a2e02737ce1675017d34e67defe12698fd69e2ac
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-20 22:01:33 +0300

    Displayed Questions

---
+++ react-survey/src/components/DisplayQuestions.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const DisplayQuestions = ({ question }) => {
+//    console.log(question)
+  return (<div>{question.question}</div>)
+      
+  }
+
+export default DisplayQuestions;
+
